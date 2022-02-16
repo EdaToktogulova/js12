37ddcd1f38fb6b83bc4f65783d28ab26ca923e55
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-16 14:47:33 +0600

    added new element

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 document.querySelector('input[type="text"]')
 .addEventListener('focus', function () {
-   this.value = "Hello";
+   this.style.borderColor = "initial";
 });
 document.querySelector('input[type="text"]' )
 .addEventListener('blur', function () {
-   this.value = "Bye";
+   if (!this.valie) {
+      this.style.borderColor = "red";
+   }
 });
 
+document.querySelector('input[type="text"]')
+.addEventListener('change', function () {
+   alert('Change')
+});
+
+document.querySelector('input[type="checkbox"]' )
+.addEventListener('blur', function () {
+   if (!this.valie) {
+      alert
+   }
+});
+
+document.querySelector('input[type="checkbox"]')
+.addEventListener('change', function () {
+   alert('Change')
+});
